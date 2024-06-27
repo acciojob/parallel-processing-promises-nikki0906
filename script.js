bb5f82164fef804737c035b1adcabf88013910aa
nikki0906 <160705654+nikki0906@users.noreply.github.com>
2024-06-27 11:40:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //your JS code here. If required.
-/*const output = document.getElementById("output");
+
+const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 
 const images = [
@@ -7,3 +8,34 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+
+btn.addEventListener('click', () => {
+      downloadAndDisplayImages(images);
+    });
+
+    function downloadImage(image) {
+      return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.src = image.url;
+        img.onload = () => resolve(img);
+        img.onerror = () => reject(new Error(`Failed to load image's URL: ${image.url}`));
+      });
+    }
+
+    function downloadAndDisplayImages(images) {
+      output.innerHTML = '';
+
+      const downloadPromises = images.map(image => downloadImage(image));
+
+      Promise.all(downloadPromises)
+        .then(images => {
+          images.forEach(img => {
+            output.appendChild(img);
+          });
+        })
+        .catch(error => {
+          console.error(error);
+          alert(error.message);
+        });
+    }
